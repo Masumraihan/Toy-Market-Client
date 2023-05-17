3c6118c45b8cbfcb7244dbc257ff736bea0f5fa9
Masumraihan <mdmasumraihan1@gamil.com>
2023-05-18 00:17:23 +0600

    start learlab and added navbar

---
+++ src/layouts/Root.jsx
@@ -0,0 +1,13 @@
+import Footer from "../shared/Footer/Footer";
+import Navbar from "../shared/Navbar/Navbar";
+
+const Root = () => {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+};
+
+export default Root;
---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+
+
+const PrivateRoute = (children) => {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+};
+
+export default PrivateRoute;
